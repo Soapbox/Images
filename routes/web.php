b12ad00f7a0e4fe0c7a8db125cce633ef4e48f44
Graham McCarthy <graham@soapboxhq.com>
2018-04-20 15:18:31 -0400

    :apple: add support for emojii and unicode characters

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use Emojione\Client;
+use Emojione\Ruleset;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -23,32 +25,48 @@ Route::get('/i/{image}', function ($image) {
 
     $img = Image::make(storage_path(sprintf('app/%s', $file)));
     
+    //if the user has entered text to render
     if (strlen($image) > 0) {
-        //only retain first character
-        $imageStr = mb_substr($image,0,1);
+        $imageStr = mb_substr($image,0,1); //grab only the first character
         
-        //if alphanumeric - capitalize and use Open Sans
-        //if non-alphanumeric - do not capitalize and use Shitty Arial Unicode font
+        //if alphanumeric - capitalize and use Open Sans. Cause it's a dope font
         if (ctype_alnum($imageStr)) {
             $imageStr = mb_convert_case($imageStr, MB_CASE_UPPER);
             
             $img->text($imageStr, 96, 96, function($font) {
                 $font->file(storage_path('open-sans.bold.ttf'));
-                $font->size(160);
+                $font->size(136);
                 $font->color('#fff');
                 $font->align('center');
                 $font->valign('middle');
             });  
         } else {
-            $img->text($imageStr, 96, 96, function($font) {
-                $font->file(storage_path('Arial Unicode MS.ttf'));
-                $font->size(160);
-                $font->color('#fff');
-                $font->align('center');
-                $font->valign('middle');
-            });  
-        }
-       
+            //if non-alphanumeric
+            
+            //if is emojii, render the image.
+            $emojione = new Client(new Ruleset());
+            $emojione->emojiSize = 128;
+            $shortCode = $emojione->toShort($imageStr);
+
+            if(mb_strlen($shortCode) > 1) {
+                $emoji = $emojione->ToImage($imageStr);
+                $xpath = new DOMXPath(@DOMDocument::loadHTML($emoji));
+                $src = $xpath->evaluate("string(//img/@src)");
+                $emojiImage = Image::make($src);
+                $emojiImage->resize(100, 100);
+                $img->insert($emojiImage, 'center');
+            } else {
+                //if it is not an emoji but is special 
+                //do not capitalize and use Shitty Arial Unicode Font
+                $img->text($imageStr, 96, 96, function($font) {
+                    $font->file(storage_path('Arial Unicode MS.ttf'));
+                    $font->size(110);
+                    $font->color('#fff');
+                    $font->align('center');
+                    $font->valign('middle');
+                }); 
+            }
+        } 
     }
 
     return $img->response('png');
