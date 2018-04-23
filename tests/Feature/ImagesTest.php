93494eea7c1c96d170b91a76748c7ff29cc62a75
Jaspaul Bola <jaspaul.b@gmail.com>
2018-04-23 10:37:48 -0400

    Refactors the images service
    This change just moves code around and introduces some helper
    methods in an attempt to make our code a little more readable.
    
    The majority of the complexity has been moved into an image filter
    that we can apply to an image.
    
    This also introduces some basic tests for the routes just as a
    quick check to see if anything is broken.

---
+++ tests/Feature/ImagesTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Http\Response;
+
+class Images extends TestCase
+{
+    /**
+     * @test
+     */
+    public function it_returns_a_404_if_you_attempt_to_produce_an_image_with_an_empty_string()
+    {
+        $response = $this->get('/i/');
+        $response->assertStatus(Response::HTTP_NOT_FOUND);
+    }
+
+    /**
+     * @test
+     */
+    public function it_returns_a_success_response_if_you_attempt_to_produce_an_image_with_a_string()
+    {
+        $response = $this->get('/i/test');
+        $response->assertStatus(Response::HTTP_OK);
+    }
+
+    /**
+     * @test
+     */
+    public function it_returns_a_success_response_if_you_attempt_to_produce_an_image_with_a_string_starting_with_😆()
+    {
+        $response = $this->get('/i/😆');
+        $response->assertStatus(Response::HTTP_OK);
+    }
+
+    /**
+     * @test
+     */
+    public function it_returns_a_success_response_if_you_attempt_to_produce_an_image_with_a_string_starting_with_汉()
+    {
+        $response = $this->get('/i/汉');
+        $response->assertStatus(Response::HTTP_OK);
+    }
+}
